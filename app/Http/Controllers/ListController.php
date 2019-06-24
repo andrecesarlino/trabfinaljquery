d5351fa0e65cc5e8c8af4be250bb8d698ae8fa9c
Andre <andrelino@unidavi.edu.br>
2019-06-24 15:30:40 -0300

    teste
    teste

--- app/Http/Controllers/ListController.php
+++ app/Http/Controllers/ListController.php
@@ -8,31 +8,25 @@ use App\Item;
 class ListController extends Controller
 {
 
-    public function index()
-    {
+    public function index() {
     	$items = Item::all();
     	return view('list', compact('items'));
     }
 
-    public function create(Request $request)
-    {
-    	/*return $request->all(); this returns the input data from the text field*/
+    public function create(Request $request) {
     	$item = new Item;
     	$item->item = $request->field;
     	$item->save();
     	return "done";
     }
 
-    public function delete(Request $request)
-    {
-    	/*return $request->all();*/
+    public function delete(Request $request) {
     	$item = Item::where('id', $request->field);
     	$item->delete();
     }
 
-    public function update(Request $request)
-    {
-    	/*return $request->all();*/
+    public function update(Request $request) {
+
     	$item = Item::find($request->id);
     	$item->item = $request->value;
     	$item->save();
